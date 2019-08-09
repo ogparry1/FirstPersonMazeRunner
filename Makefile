5ad16170e4fb1b56341a7c38bcb668933878328b
John Parry <ogparry1@ufl.edu>
2019-08-09 00:43:57 -0400

    Restarted Project

--- Makefile
+++ Makefile
@@ -1,6 +1,25 @@
 BINS=MazeEscape
-SRC_FILES=src/*.cpp
-OBJ_FILES=obj/*.o
+SRC_DIR=src
+OBJ_DIR=obj
+INC_DIR=include
+SRC_FILES=$(foreach dir, $(SRC_DIR), $(wildcard $(dir)/*.cpp))
+OBJ_FILES=$(foreach dir, $(OBJ_DIR), $(wildcard $(dir)/*.o))
+INC_FILES=$(foreach dir, $(INC_DIR), $(wildcard $(dir)/*.hpp))
 
-all: 
-	g++ -pthread -o ${BINS} ${SRC_FILES}
+CXX=g++
+CFLAGS= -c -g
+LDFLAGS=-pthread
+
+all: $(BINS)
+	echo $(SRC_FILES)
+
+$(BINS): $(OBJ_FILES)
+	$(CXX) $(LDFLAGS) -o $@ $^
+
+$(OBJ_FILES): $(SRC_FILES) $(INC_FILES) 
+	$(CXX) $(CFLAGS) -o $@ $(SRC_FILES)
+
+.PHONY: clean
+
+clean:
+	rm -f $(OBJ_FILES) $(BINS)
--- src/MazeEscape.cpp
+++ src/MazeEscape.cpp
@@ -3,35 +3,91 @@
 #include <string> // string
 #include <thread> // thread
 #include <chrono> // thread
+#include <cmath> // sin, cos
 
 #include "../include/MazeRunner.hpp"
+
+#define PI 3.14159265
 
 using namespace Maze;
 
 int main(int argc, char **argv)
 {
-    int width = argv[1], height = argv[2];
-    int start[2] = {0,0}, end[2] = {0,0};
+    int width = std::atoi(argv[1]), 
+	height = std::atoi(argv[2]), 
+	degFOV = std::atoi(argv[3]),
+	dir = 0;
+    int rays[width] = {0};
+    int S[] = {0,0},
+	E[] = {0,0};
+    double P[] = {0,0};
+
     std::string map[10] = 
     {
-	"#E########",
-	"#.#......#",
-	"#.#.####.#",
-	"#.#.#S.#.#",
-	"#.#.##.#.#",
-	"#.#....#.#",
-	"#.#....#.#",
-	"#.######.#",
+	"##########",
+	"########.E",
 	"#........#",
+	"######...#",
+	"#........#",
+	"#........#",
+	"#....#...#",
+	"#..S.#...#",
+	"#....#...#",
 	"##########"
     };
-    for (int ii = 0; ii < arrSize(map); ii++)
+
+    for (int ii = 0; ii < 10; ii++)
     {
-	for (int jj = 0; jj < arrSize(map); jj++)
+	for (int jj = 0; jj < 10; jj++)
 	{
+	    if (map[ii][jj] == 'S')
+	    {
+		S[0] = ii;
+		S[1] = jj;
+	    }
+	    else if (map[ii][jj] == 'E')
+	    {
+		E[0] = ii;
+		E[1] = jj;
+	    }
+	    else if (map[ii][jj] == 'P')
+	    {
+		P[0] = ii+0.5;
+		P[1] = jj+0.5;
+	    }
+	    else {}
 	}
     }
 
-    MazeRunner runner(100, 100);
+    const double raystep = 0.5;
+    while (true)
+    {
+	int deg = dir-degFOV/2;
+	double raypos[2];
+	int max = 0;
+
+	// Ray Trace
+	for (int ii = 0; ii < width; ii++)
+	{
+	    raypos[0] = P[0];
+	    raypos[1] = P[1];
+	    rays[ii] = 0;
+	    while (map[(int)raypos[0]][(int)raypos[1]] != '#')
+	    {
+		raypos[0] += raystep*std::cos(deg*PI/180);
+		raypos[1] += raystep*std::sin(deg*PI/180);
+		rays[ii] += 1;
+	    }
+	    if (max < rays[ii]) max = rays[ii];
+	    deg += degFOV/width;
+	}
+
+	// Print to Screen
+	for (int ii = 0; ii < width; ii++)
+	{
+	}
+
+	// Check for user input and adjust
+    }
     return 0;
 }
